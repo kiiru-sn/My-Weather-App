9554d49047da88b77227beafce5a4cf6c898900a
kiiru-sn <kiiru.sn@gmail.com>
2023-05-19 17:55:28 +0100

    Added my weather project from VSCode (index, css, script)

---
+++ src/script.js
@@ -0,0 +1,100 @@
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = days[date.getDay()];
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+let currentInfo = document.querySelector("#dayAndTime");
+let currentTime = new Date();
+currentInfo.innerHTML = formatDate(currentTime);
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#number").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  console.log(response.data);
+}
+
+function search(city) {
+  let apiKey = "a710bd8bd76400c9658ef649d9e81728";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  search(city);
+}
+
+let form = document.querySelector("#searchBar");
+form.addEventListener("submit", handleSubmit);
+
+search("San Diego");
+
+function searchLocation(position) {
+  let apiKey = "a710bd8bd76400c9658ef649d9e81728";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#cLButton");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+//function changeF(event) {
+//event.preventDefault();
+//let number = document.querySelector("#number");
+//let temperature = number.innerHTML;
+//number.innerHTML = Math.round((temperature * 9) / 5 + 32);
+//}
+
+//function changeC(event) {
+//event.preventDefault();
+//let number = document.querySelector("#number");
+//let temperature = number.innerHTML;
+//number.innerHTML = Math.round(((temperature - 32) * 5) / 9);
+//}
+
+//let fahrenheit = document.querySelector("#f");
+//fahrenheit.addEventListener("click", changeF);
+
+//let celcius = document.querySelector("#c");
+//celcius.addEventListener("click", changeC);
+
+////////////////////
+
+//let lat = "53.2011119";
+//let lon = "-6.5738043";
+//let part = "current";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}&units=metric`;
+
+//function showTemperature(response) {}
+
+//axios.get(apiUrl).then(showTemperature);
